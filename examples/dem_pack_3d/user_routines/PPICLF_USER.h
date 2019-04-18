dd46b995f876a353e0c09c24ab4a69fef88d7e45
dpzwick <dpzwick@gmail.com>
2019-04-18 10:20:04 -0400

    Rearranged PPICLF_USER.h file for understanding

--- examples/dem_pack_3d/user_routines/PPICLF_USER.h
+++ examples/dem_pack_3d/user_routines/PPICLF_USER.h
@@ -1,14 +1,13 @@
 #define PPICLF_LRS 6
+#define PPICLF_LRP 3
+#define PPICLF_LWALL 800
+
 #define PPICLF_JX  1
 #define PPICLF_JY  2
 #define PPICLF_JZ  3
 #define PPICLF_JVX 4
 #define PPICLF_JVY 5
 #define PPICLF_JVZ 6
-
-#define PPICLF_LRP 3
 #define PPICLF_R_JRHOP 1
 #define PPICLF_R_JDP   2
 #define PPICLF_R_JVOLP 3
-
-#define PPICLF_LWALL 800
--- examples/stokes_2d/user_routines/PPICLF_USER.h
+++ examples/stokes_2d/user_routines/PPICLF_USER.h
@@ -1,8 +1,8 @@
-#define PPICLF_LRS 4
+#define PPICLF_LRS 4 
+#define PPICLF_LRP 1 
+
 #define PPICLF_JX  1
 #define PPICLF_JY  2
 #define PPICLF_JVX 3
 #define PPICLF_JVY 4
-
-#define PPICLF_LRP 1
 #define PPICLF_R_JTAUP 1
